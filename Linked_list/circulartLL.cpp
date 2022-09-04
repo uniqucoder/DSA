75d6fb5c413f06da82e91b9eb9ba9a6febc50b48
uniqucoder <chaitanyatupsamudre@gmail.com>
2022-09-04 09:36:16 +0530

    CLL

--- Linked_list/circulartLL.cpp
+++ Linked_list/circulartLL.cpp
@@ -23,28 +23,85 @@ void traverse(Node* &head)
 
     do
     {
-        /* code */
+        
         cout<<temp ->data<<" ";
         temp = temp ->next;
     } while (temp!=head);
     
 }
 
+//insert  at empty list
+
+void insertEmpty(Node* &head,Node* &tail, int d)
+{
+    if(head == NULL)
+    {
+        Node* newNode = new Node(d);
+        head = newNode;
+        
+        tail = head;
+        tail ->next = head;
+    }
+}
+
+//insert at first node
+
+void insertFirst(Node* &head,Node* &tail, int d)
+{
+
+    Node* newNode = new Node(d);
+
+    newNode ->next = head;
+    head = newNode;
+    tail -> next = head;
+
+}
+
+// insert after 
+void insertafter(Node* &head, Node* & tail , int target, int d)
+{
+    Node* temp = head;
+    while(temp != NULL && temp ->data != target)
+    {
+        temp = temp ->next;
+
+    }
+   
+    Node* newNode = new Node(d);
+    if(temp == NULL)
+    {
+      tail = newNode;
+      tail->next = head;  
+    } 
+    newNode ->next = temp ->next;
+    temp->next = newNode;
+    
+}
+
+// insert at end
+
+void insertEnd(Node* &head,Node* &tail, int d)
+{
+    Node* newNode = new Node(d);
+    tail ->next = newNode;
+    tail = newNode;
+    tail ->next = head;
+}
 
 int main()
 {
-    Node* first = new Node(1);
 
+    Node* head = NULL;
+    Node* tail = NULL;
 
-
-    Node* head = first;
-    Node* tail = first;
-
+    insertEmpty(head, tail, 1);
+    insertEnd(head, tail,5);
+    insertafter(head,tail, 1,2);
+    insertafter(head,tail,2,3);
+    // insertafter(head,tail, 1,2);
+    insertafter(head,tail,2,10);
     
-
+    traverse(head);
     
-    traverse(first);
-
-
 
 }
